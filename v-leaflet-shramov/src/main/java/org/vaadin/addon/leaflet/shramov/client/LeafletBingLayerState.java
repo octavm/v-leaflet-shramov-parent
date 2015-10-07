d2409ed9643d2de065a72dc831b550726138e4fd
octavm <octavm@startmail.com>
2015-10-07 10:27:34 +0200

    Create LeafletBingLayerState.java

---
+++ v-leaflet-shramov/src/main/java/org/vaadin/addon/leaflet/shramov/client/LeafletBingLayerState.java
@@ -0,0 +1,8 @@
+package org.vaadin.addon.leaflet.shramov.client;
+
+import org.vaadin.addon.leaflet.shared.AbstractLeafletComponentState;
+
+public class LeafletBingLayerState extends AbstractLeafletComponentState {
+    public String layertype;
+    public String key;
+}

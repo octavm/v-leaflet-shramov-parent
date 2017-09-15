67cdf0511948e3e1f5eb532f275ff6d09db94bac
octavm <octavm@startmail.com>
2017-09-15 16:10:07 +0200

    Upgraded to vaadin-8 and removed dependency to testbench so everyone can run tests

---
+++ v-leaflet-shramov/src/test/java/org/vaadin/addon/leaflet/demoandtestapp/util/UiRunner.java
@@ -0,0 +1,17 @@
+/*
+ * $Id:  $
+ * 
+ *  Copyright (C) 2015, CGI Group Inc. All rights reserved.
+ */
+package org.vaadin.addon.leaflet.demoandtestapp.util;
+
+import com.vaadin.annotations.Widgetset;
+import org.vaadin.addonhelpers.TServer;
+
+@Widgetset("org.vaadin.addon.leaflet.demoandtestapp.TestWidgetset")
+public class UiRunner extends TServer {
+
+    public static void main(String[] args) throws Exception {
+        new UiRunner().startServer();
+    }
+}
--- v-leaflet-shramov/src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
+++ v-leaflet-shramov/src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
@@ -1,18 +1,18 @@
 package org.vaadin.addon.leaflet.testbenchtests;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.vaadin.addon.leaflet.demoandtestapp.util.TListUi;
+import org.vaadin.addonhelpers.TListUi;
+import org.vaadin.addonhelpers.automated.AbstractWebDriverCase;
 
-import com.vaadin.data.Container;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class SimpleTest extends AbstractTestBenchTest {
+public class SimpleTest extends AbstractWebDriverCase {
 
     @Test
     public void checkAllTestsOpenWithoutErrors() throws IOException, AssertionError {
@@ -20,15 +20,14 @@ public class SimpleTest extends AbstractTestBenchTest {
 
         driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
         
-        Container listTestClasses = TListUi.listTestClasses();
-        for (Object id : listTestClasses.getItemIds()) {
-			String name = (String) listTestClasses.getItem(id)
-					.getItemProperty("name").getValue();
+        List<TListUi.TestDetails> listTestClasses = TListUi.listTestClasses();
+        for (TListUi.TestDetails td : listTestClasses) {
+        	Class clazz = td.getClazz();
 			
-			driver.get(BASEURL + name + "?debug");
+			driver.get(BASEURL + clazz.getName() + "?debug");
 			try {
 				WebElement error = driver.findElement(By.className("v-Notification-error"));
-				Assert.fail("Test " + name + " has client side exception");
+				Assert.fail("Test " + clazz.getName() + " has client side exception");
 			} catch (NoSuchElementException e) {
 				continue;
 			}
